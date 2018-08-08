feb7c801b0a83905f6f1ebfb5a56c5dc53760de1
Ruslan <4ruslan.k@gmail.com>
2018-08-08 11:07:24 +0500

    banner component

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Listing() {
+  return (
+    <div className="banner">
+      <div>
+        <h2>Ruslan Khametov</h2>
+        <p>Full Stack Developer</p>
+      </div>
+    </div>
+  );
+}
