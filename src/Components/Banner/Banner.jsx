d04f3dda1863c05d7b332d810534c8751ee119f5
ruslan <4ruslan.k@gmail.com>
2020-10-11 16:15:01 +0500

    background video for top section

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import TextLoop from 'react-text-loop';
 import ContactIcons from '../ContactIcons/ContactIcons';
-import './Banner.css';
+import './Banner.scss';
 
 export default function Banner() {
+  const videoRef = useRef(null);
+  const setPlayBack = () => {
+    videoRef.current.playbackRate = 0.7;
+  };
   return (
     <div className="banner-container">
+      <div className="video-background">
+        <video
+          onCanPlay={() => setPlayBack()}
+          y
+          ref={videoRef}
+          autoPlay
+          muted
+          playsInline
+          loop
+          src={`${'/assets/images/service/skyCamp.mp4'}`}
+        />
+      </div>
       <div className="banner">
-        <div>
+        <div className="d-inline-flex flex-column justify-content-center align-items-center">
           <h2>Ruslan Khametov</h2>
-          <p>Full Stack Developer</p>
+          <TextLoop className="theme-color text-loop">
+            <span>Full Stack Developer</span>
+            <span>Javascript</span>
+            <span>React.js</span>
+            <span>Node.js</span>
+            <span>SQL</span>
+            <span>NoSQL</span>
+          </TextLoop>
           <ContactIcons />
         </div>
       </div>
