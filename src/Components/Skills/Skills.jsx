140f7fae3faf84e125f3f234329df32900d936a4
ruslan <4ruslan.k@gmail.com>
2020-10-11 17:11:07 +0500

    skills section

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,36 +1,71 @@
 import React from 'react';
-import './Skills.css';
-import SkillContainer from './SkillContainer/SkillContainer';
+import {
+  FiLayers, FiDownloadCloud, FiCode, FiDatabase, FiLayout, FiTrendingUp,
+} from 'react-icons/fi';
+import './Skills.scss';
 
-const LOGOS = [
-  ['react.svg', 'React'],
-  ['angular.svg', 'Angular'],
-  ['css.svg', 'CSS'],
-  ['html.png', 'HTML'],
-  ['redux.png', 'Redux'],
-  ['bootstrap.png', 'Bootstrap'],
-  ['node.svg', 'Node'],
-  ['mongo.svg', 'MongoDB'],
-  ['jquery.svg', 'jQuery'],
+const ServiceList = [
+  {
+    icon: <FiLayout />,
+    title: 'Front-end',
+    description: 'React, Redux, JavaScript, HTML, CSS',
+  },
+  {
+    icon: <FiLayers />,
+    title: 'Back-end',
+    description: 'Node JS, ExpressJS, Firebase, Django',
+  },
+  {
+    icon: <FiDatabase />,
+    title: 'Databases',
+    description: 'Firestore, MongoDB. DynamoDB, PostgreSQL',
+  },
+  {
+    icon: <FiDownloadCloud />,
+    title: 'Deployment',
+    description: 'Google Cloud Platform, AWS, Firebase, Heroku, Jenkins',
+  },
+  {
+    icon: <FiCode />,
+    title: 'Languages',
+    description: 'Javascript, Python',
+  },
+  {
+    icon: <FiTrendingUp />,
+    title: 'Other Technologies',
+    description: 'Sendgrid, Single Sign-On with SCIM',
+  },
 ];
-const IMAGE_NAME = 0;
-const SKILL_TITLE = 1;
-
-export default function SkillIcons() {
-  return (
-    <div className="skills-container">
-      <div>
-        <h1 style={{ color: 'white', margin: 0 }}>Skills</h1>
-        <div className="skill-icons">
-          {LOGOS.map((logo, index) => (
-            <SkillContainer
-              key={`logo-${index}`}
-              imageName={logo[IMAGE_NAME]}
-              skillTitle={logo[SKILL_TITLE]}
-            />
-          ))}
+const Skills = () => (
+  <div className="service-area ptb--120 bg_color--5">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12">
+          <div className="section-title text-center mb--30">
+            <h2>My Skills</h2>
+            <p>
+              There are many technologies that I have experience with.
+            </p>
+          </div>
         </div>
       </div>
+      <div className="row service-one-wrapper">
+        {ServiceList.map((val) => (
+          <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12" key={val}>
+            <div>
+              <div className="service service_style">
+                <div className="icon">{val.icon}</div>
+                <div className="content">
+                  <h3 className="title">{val.title}</h3>
+                  <p>{val.description}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  );
-}
+  </div>
+);
+
+export default Skills;
