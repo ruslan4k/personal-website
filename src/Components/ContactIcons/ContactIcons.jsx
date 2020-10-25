26a31d48d0c2170ab2ea4edd30a18eebc32f2684
ruslan <4ruslan.k@gmail.com>
2020-10-25 12:38:59 +0500

    delete redundant packages

--- src/Components/ContactIcons/ContactIcons.jsx
+++ src/Components/ContactIcons/ContactIcons.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import './ContactIcons.css';
-import LinkedInIcon from 'mdi-react/LinkedinIcon';
-import Github from 'mdi-react/GithubIcon';
-import CodepenIcon from 'mdi-react/CodepenIcon';
+import { AiOutlineCodepenCircle, AiOutlineLinkedin, AiOutlineGithub } from 'react-icons/ai';
 import { GITHUB_LINK, LINKEDIN_LINK, CODEPEN_LINK } from '../../constants/socialLinks';
 
 export default function ContactIcons() {
   return (
     <div className="social-icons">
       <a target="blank" href={LINKEDIN_LINK}>
-        <LinkedInIcon size={36} className="social-icon linkedIn" />
+        <AiOutlineLinkedin size={36} className="social-icon linkedIn" />
       </a>
       <a target="blank" href={GITHUB_LINK}>
-        <Github size={36} className="social-icon github" />
+        <AiOutlineGithub size={36} className="social-icon github" />
       </a>
       <a target="blank" href={CODEPEN_LINK}>
-        <CodepenIcon size={36} className="social-icon" />
+        <AiOutlineCodepenCircle size={36} className="social-icon" />
       </a>
     </div>
   );
