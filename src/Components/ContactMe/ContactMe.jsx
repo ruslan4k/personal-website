93b7fbe9dd0520d7dc1fb2d04ae7681f253d87c7
ruslan <4ruslan.k@gmail.com>
2020-10-25 12:31:28 +0500

    update contact form

--- src/Components/ContactMe/ContactMe.jsx
+++ src/Components/ContactMe/ContactMe.jsx
@@ -1,97 +1,115 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Formik } from 'formik';
+import * as yup from 'yup';
 import './ContactMe.scss';
+import { FormGroup, Input, FormFeedback, Form } from 'reactstrap';
 
-const ContactMe = () => {
-  const [state, setState] = useState({
-    rnName: '',
-    rnEmail: '',
-    rnSubject: '',
-    rnMessage: '',
-  });
-
-  return (
-    <div id="contact" className="contact-form-styles bg_color--5">
-      <div className="container">
-        <div className="row row--35 align-items-center">
-          <div className="col-lg-6 order-2 order-lg-1">
-            <div className="section-title text-left mb--50">
-              <h2 className="title">Contact Me</h2>
-              {/* <p className="description">
-                Lorem ipsum dolor
-              </p> */}
-            </div>
-            <div className="form-wrapper">
-              <form>
-                <label htmlFor="item01">
-                  <input
-                    type="text"
-                    name="name"
-                    id="item01"
-                    value={state.rnName}
-                    onChange={(e) => {
-                      setState({ rnName: e.target.value });
-                    }}
-                    placeholder="Your Name *"
-                  />
-                </label>
-
-                <label htmlFor="item02">
-                  <input
-                    type="text"
-                    name="email"
-                    id="item02"
-                    value={state.rnEmail}
-                    onChange={(e) => {
-                      setState({ rnEmail: e.target.value });
-                    }}
-                    placeholder="Your email *"
-                  />
-                </label>
-
-                <label htmlFor="item03">
-                  <input
-                    type="text"
-                    name="subject"
-                    id="item03"
-                    value={state.rnSubject}
-                    onChange={(e) => {
-                      setState({ rnSubject: e.target.value });
-                    }}
-                    placeholder="Write a Subject"
-                  />
-                </label>
-                <label htmlFor="item04">
-                  <textarea
-                    type="text"
-                    id="item04"
-                    name="message"
-                    value={state.rnMessage}
-                    onChange={(e) => {
-                      setState({ rnMessage: e.target.value });
-                    }}
-                    placeholder="Your Message"
-                  />
-                </label>
-                <button
-                  className="rn-button-style--2 btn-solid"
-                  type="submit"
-                  value="submit"
-                  name="submit"
-                  id="mc-embedded-subscribe"
-                >
-                  Submit
-                </button>
-              </form>
-            </div>
+const ContactMe = () => (
+  <div id="contact" className="contact-form-styles bg_color--5">
+    <div className="container">
+      <div className="row row--35 align-items-center">
+        <div className="col-lg-6 order-2 order-lg-1">
+          <div className="section-title text-left mb--50">
+            <h2 className="title">Contact Me</h2>
           </div>
-          <div className="col-lg-6 order-1 order-lg-2">
-            <div className="thumbnail mb_md--30 mb_sm--30">
-              <img src="/assets/images/letsContact.png" alt="illustration" />
-            </div>
+          <div className="form-wrapper">
+            <Formik
+              initialValues={{
+                email: '',
+                name: '',
+                subject: '',
+                message: '',
+              }}
+              validationSchema={yup.object().shape({
+                email: yup.string().email('Email not valid').required('Email is required'),
+                name: yup.string().required('Name is required!'),
+                subject: yup.string().required('Subject is required!'),
+                message: yup.string().required('Message is required!'),
+              })}
+              onSubmit={async (values) => {
+                try {
+                  await fetch('https://us-central1-fake-data-generator-292318.cloudfunctions.net/contact_form_request', {
+                    method: 'POST',
+                    body: JSON.stringify(values),
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                  });
+                } catch (error) {
+                  console.log('error', error);
+                }
+              }}
+              render={({ errors, touched, handleChange, values, handleSubmit, handleBlur }) => (
+                <Form onSubmit={handleSubmit}>
+                  <FormGroup>
+                    <Input
+                      type="text"
+                      placeholder="Enter your email"
+                      value={values.email}
+                      onChange={handleChange}
+                      name="email"
+                      onBlur={handleBlur}
+                      invalid={touched.email && errors.email}
+                    />
+                    <FormFeedback>{errors.email}</FormFeedback>
+                  </FormGroup>
+                  <FormGroup>
+                    <Input
+                      type="text"
+                      name="name"
+                      onChange={handleChange}
+                      value={values.name}
+                      placeholder="Enter your Name"
+                      onBlur={handleBlur}
+                      invalid={touched.name && errors.name}
+                    />
+                    <FormFeedback>{errors.name}</FormFeedback>
+                  </FormGroup>
+                  <FormGroup>
+                    <Input
+                      type="text"
+                      name="subject"
+                      onChange={handleChange}
+                      value={values.subject}
+                      placeholder="Write a Subject"
+                      onBlur={handleBlur}
+                      invalid={touched.subject && errors.subject}
+                    />
+                    <FormFeedback>{errors.subject}</FormFeedback>
+                  </FormGroup>
+                  <FormGroup>
+                    <Input
+                      type="textarea"
+                      name="message"
+                      placeholder="Enter your Message"
+                      onChange={handleChange}
+                      value={values.message}
+                      onBlur={handleBlur}
+                      invalid={touched.message && errors.message}
+                    />
+                    <FormFeedback>{errors.message}</FormFeedback>
+                  </FormGroup>
+                  <button
+                    className="rn-button-style--2 btn-solid"
+                    type="submit"
+                    value="submit"
+                    name="submit"
+                    id="mc-embedded-subscribe"
+                  >
+                    Submit
+                  </button>
+                </Form>
+              )}
+            />
+          </div>
+        </div>
+        <div className="col-lg-6 order-1 order-lg-2">
+          <div className="thumbnail mb_md--30 mb_sm--30">
+            <img src="/assets/images/letsContact.png" alt="illustration" />
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 export default ContactMe;
