9067ae283c00ef584e78ae2197ca70ffa45a7576
ruslan <4ruslan.k@gmail.com>
2020-11-01 21:09:52 +0500

    make the whole project item card clickable

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from 'react-slick';
 import { portfolioSlick } from './config';
 import activityTimerImg from '../../assets/images/projects/activityTimer/4.png';
@@ -93,42 +93,72 @@ const projects = [
   },
 ];
 
-const Portfolio = () => (
-  <div id="projects" className="portfolio-area ptb--120 bg_color--1">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-6">
-          <div className="section-title text-left service-style--3 mb--30">
-            <h2 className="title">My Projects</h2>
+const Portfolio = () => {
+  const [isModalOpen, setOpenModal] = useState(false);
+  const [isLightBoxOpen, setLightBoxOpen] = useState(false);
+  const [currentProject, setProject] = useState(projects[0]);
+  const toggleModalState = () => {
+    if (isLightBoxOpen) return;
+    setOpenModal((state) => !state);
+  };
+
+  const changeLightBoxStatus = () => {
+    setLightBoxOpen((state) => !state);
+  };
+
+  const handleClickProject = (project) => {
+    setProject(project);
+    toggleModalState();
+  };
+
+  return (
+    <div id="projects" className="portfolio-area ptb--120 bg_color--1">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="section-title text-left service-style--3 mb--30">
+              <h2 className="title">My Projects</h2>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className="wrapper portfolio-carousel-inner mb--55">
-      <div className="portfolio-slick-activation mt--30 mt_sm--30">
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        <Slider {...portfolioSlick}>
-          {projects.map((projectItem) => (
-            <div className="portfolio portfolio-interior-design" key={projectItem.image}>
-              <div className="thumbnail-inner">
-                <div className={`thumbnail ${projectItem.image}`} />
-                <div className={`bg-blr-image ${projectItem.image}`} />
-              </div>
-              <div className="content">
-                <div className="inner d-flex flex-column align-items-start">
-                  <p>{projectItem.shortDescription}</p>
-                  <h4>
-                    <a href="/portfolio-details">{projectItem.title}</a>
-                  </h4>
-                  <ProjectModal project={projectItem} />
+      <div className="wrapper portfolio-carousel-inner mb--55">
+        <div className="portfolio-slick-activation mt--30 mt_sm--30">
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          <Slider {...portfolioSlick}>
+            {projects.map((projectItem) => (
+              <div
+                tabIndex={-1}
+                role="button"
+                className="portfolio portfolio-interior-design"
+                key={projectItem.image}
+                onKeyDown={() => handleClickProject(projectItem)}
+                onClick={() => handleClickProject(projectItem)}
+              >
+                <div className="thumbnail-inner">
+                  <div className={`thumbnail ${projectItem.image}`} />
+                  <div className={`bg-blr-image ${projectItem.image}`} />
+                </div>
+                <div className="content">
+                  <div className="inner d-flex flex-column align-items-start">
+                    <p>{projectItem.shortDescription}</p>
+                    <h4 className="text-white">{projectItem.title}</h4>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Slider>
+        </div>
       </div>
+      <ProjectModal
+        project={currentProject}
+        isModalOpen={isModalOpen}
+        isLightBoxOpen={isLightBoxOpen}
+        changeLightBoxStatus={changeLightBoxStatus}
+        toggleModalState={toggleModalState}
+      />
     </div>
-  </div>
-);
+  );
+};
 
 export default Portfolio;
--- src/Components/Portfolio/ProjectModal/ProjectModal.jsx
+++ src/Components/Portfolio/ProjectModal/ProjectModal.jsx
@@ -1,30 +1,17 @@
 /* eslint-disable react/forbid-prop-types */
-import React, { useState } from 'react';
+import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import './ProjectModal.css';
-import { object } from 'prop-types';
+import { bool, func, object } from 'prop-types';
 import Gallery from '../Gallery/Gallery';
 
-const ProjectModal = ({ project }) => {
+const ProjectModal = ({ project, isModalOpen, toggleModalState, changeLightBoxStatus }) => {
   const { images, title, description, url } = project;
-  const [modal, setOpenModal] = useState(false);
-  const [isLightBoxOpen, setLightBoxOpen] = useState(false);
-  const toggle = () => {
-    if (isLightBoxOpen) return;
-    setOpenModal((state) => !state);
-  };
-
-  const changeLightBoxStatus = () => {
-    setLightBoxOpen((state) => !state);
-  };
 
   return (
     <div>
-      <div className="portfolio-button" onClick={toggle} onKeyDown={toggle} role="button" tabIndex={-1}>
-        <a className="rn-btn">Show Details</a>
-      </div>
-      <Modal size="lg" centered isOpen={modal} toggle={toggle}>
-        <ModalHeader toggle={toggle}>{title}</ModalHeader>
+      <Modal size="xl" centered isOpen={isModalOpen} toggle={toggleModalState}>
+        <ModalHeader toggle={toggleModalState}>{title}</ModalHeader>
         <ModalBody>{description}</ModalBody>
         <ModalBody>
           <Gallery images={images} changeLightBoxStatus={changeLightBoxStatus} />
@@ -35,7 +22,7 @@ const ProjectModal = ({ project }) => {
               <Button color="primary">Visit Website</Button>
             </a>
           )}
-          <Button color="secondary" onClick={toggle}>
+          <Button color="secondary" onClick={toggleModalState}>
             Close
           </Button>
         </ModalFooter>
@@ -46,6 +33,9 @@ const ProjectModal = ({ project }) => {
 
 ProjectModal.propTypes = {
   project: object.isRequired,
+  isModalOpen: bool.isRequired,
+  toggleModalState: func.isRequired,
+  changeLightBoxStatus: func.isRequired,
 };
 
 export default ProjectModal;
